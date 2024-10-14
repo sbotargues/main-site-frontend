4b750e68134f61584895b9864b19f69339085700
Sergi Botargues <sbotargues@gmail.com>
2024-10-15 00:29:42 +0200

    first-commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="min-height: 100vh; background-color: lightgray">
+    <TaskApp />
+  </div>
+</template>
+
+<script setup>
+import TaskApp from "taskManagerFrontend/TaskApp";
+
+// Verifica si el componente llega correctamente
+console.log("TaskApp:", TaskApp);
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineNuxtConfig({
+  ssr: false,
+  vite: {
+    plugins: [
+      federation({
+        remotes: {
+          taskManagerFrontend:
+            "taskManagerFrontend@http://localhost:3000/_nuxt/remoteEntry.js",
+        },
+        shared: ["vue"],
+      }),
+    ],
+  },
+
+  compatibilityDate: "2024-10-15",
+});
